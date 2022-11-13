e59293ae96196cf7eb37e41a9deec3b8430af3f4
nataliakoliveira <nataliaakellyy@gmail.com>
2022-11-13 19:00:51 -0300

    tela cadastro

---
+++ src/pages/cadastro/index.jsx
@@ -0,0 +1,70 @@
+import { useNavigate  } from "react-router-dom";
+import { MdEmail, MdLock } from 'react-icons/md'
+import { Button } from '../../components/Button';
+import { Header } from '../../components/Header';
+import { Input } from '../../components/Input';
+import { api } from '../../services/api';
+
+import { useForm } from "react-hook-form";
+
+
+import { Container, Title, Column, TitleLogin, SubtitleLogin, CriarText, Row, Wrapper, TextContent } from './styles';
+
+const Cadastro = () => {
+
+    const navigate = useNavigate()
+
+    const { control, handleSubmit, formState: { errors  } } = useForm({
+        reValidateMode: 'onChange',
+        mode: 'onChange',
+    });
+
+    const onSubmit = async (formData) => {
+        try{
+            const {data} = await api.get(`/users?email=${formData.email}&senha=${formData.senha}`);
+            
+            if(data.length && data[0].id){
+                navigate('/feed') 
+                return
+            }
+
+            alert('Usuário ou senha inválido')
+        }catch(e){
+            //TODO: HOUVE UM ERRO
+        }
+    };
+
+    console.log('errors', errors);
+
+    return (<>
+        <Header />
+        <Container>
+            <Column>
+                <Title>A plataforma para você aprender com experts, dominar as principais tecnologias
+                 e entrar mais rápido nas empresas mais desejadas.</Title>
+            </Column>
+            <Column>
+                <Wrapper>
+                <TitleLogin>Comece agora grátis</TitleLogin>
+                <SubtitleLogin>Crie sua conta e make the change._</SubtitleLogin>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                <Input placeholder="Nome completo" leftIcon={<MdEmail />} name="Nome"  control={control} />
+                    {errors.nome && <span>Nome é obrigatório</span>}
+                    <Input placeholder="E-mail" leftIcon={<MdEmail />} name="email"  control={control} />
+                    {errors.email && <span>E-mail é obrigatório</span>}
+                    <Input type="password" placeholder="Senha" leftIcon={<MdLock />}  name="senha" control={control} />
+                    {errors.senha && <span>Senha é obrigatório</span>}
+                    <Button title="Criar minha conta" variant="secondary" type="submit"/>
+                </form>
+                <Row>
+                <TextContent>Domine as tecnologias utilizadas pelas empresas mais inovadoras do mundo e encare seu novo
+                     desafio profissional, evoluindo em comunidade com os melhores experts.</TextContent>
+                </Row>
+                Já tenho conta? <CriarText>Fazer login</CriarText>
+                </Wrapper>
+            </Column>
+        </Container>
+    </>)
+}
+
+export { Cadastro }
